d9b29e42a39c492c68ffc18191e8f39e1b3b208e
Nobi <207083552+Nobuka72@users.noreply.github.com>
2025-12-08 07:12:06 +0000

    Implement input validation and loop in calculator
    Refactor calculator to include input validation and looping for continuous use.

--- simplecalc.py
+++ simplecalc.py
@@ -1,18 +1,31 @@
-                                # A SIMPLE CALCULATOR
-print("1-Add")
-print("2-substract")
-print("3-multipication")
-print("4-division")
-print("5-remainder")
+while True:
+    print("1-Add")
+    print("2-substract")
+    print("3-multipication")
+    print("4-division")
+    print("5-remainder")
+    try:
+        option=int(input("choose an operation(between 1to5): "))
+    except ValueError:
+        print("invalid input. please enter a number between 1 and 5.")
+        continue
+    
+    if option == 5:
+        print("Exiting calculator. Goodbye!")
+        break
 
-option=int(input("choose an operation: "))
+    if option not in [1,2,3,4,5]:
+        print("Invalid option! Please choose between 1-5.")
+        continue
 
-print("you have selected option ", option)
+    try:
+        num1 = float(input("Enter first number: "))
+        num2 = float(input("Enter second number: "))
+    except ValueError:
+        print("Invalid number entered!")
+        continue
 
-if (option in [1,2,3,4,5]):
-    num1=int(input("enter first number: "))
-    num2=int(input("enter second number: "))
-
+    print("you have selected option ", option)
 
     if (option==1):
         result=num1+num2
@@ -24,11 +37,10 @@ if (option in [1,2,3,4,5]):
         result=num1/num2
     elif (option==5):
         result=num1%num2
-    print("the result is: ", result)
+    print("the calculated result is: ", result)
 
-else: 
-    print("invalid operations entered")
-
+if __name__ == "__main__":
+    main()
 
 
 
